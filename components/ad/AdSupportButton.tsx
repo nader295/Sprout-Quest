57d429d1f762cbc03ad032498324c589ea3d768e
nader295 <nadermagdy295@gmail.com>
2026-06-12 01:25:18 -0700

    Add files via upload

---
+++ components/ad/AdSupportButton.tsx
@@ -0,0 +1,4 @@
+// DEPRECATED: This component has been replaced by SupportDevAdButton (components/shared/SupportDevAdButton.tsx)
+// This file is kept empty to avoid import errors from stale builds.
+// Safe to delete.
+export {};
